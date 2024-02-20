b95a7c3d3bd8f71695bb9a37de8d8f221a7b8340
Rahul Singh <rahuls0599@gmail.com>
2024-02-20 17:15:39 +0530

    Adding Templates with Dynamic Data Loading

--- dj_project/blog/urls.py
+++ dj_project/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="blog-home"),
-    path("about/", views.about, name="blog-about"),
+    path("", views.rendered_home, name="blog-home"),
+    path("about/", views.rendered_about, name="blog-about"),
+    path("", views.home, name="bloghome"),
+    path("about/", views.about, name="blog-about"),   
 ]
--- dj_project/blog/views.py
+++ dj_project/blog/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "CoreyMS",
+        "title": "Blog Post",
+        "content": "First post content",
+        "date_posted": "August 27, 2018"
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "August 28, 2018"
+    }
+]
+
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>Blog Home</h1>")
 
 def about(request):
     return HttpResponse("<h1>Blog About</h1>")
+
+def rendered_home(request):
+    context = {"posts": posts}
+    return render(request, 'blog/home.html', context)
+
+def rendered_about(request):
+    return render(request, "blog/about.html", {"title": "About"})
